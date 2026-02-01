9c2e70ea74f360fc4d021d67b1ff6a341bd5e39e
Artyom <kabanproff@gmail.com>
2026-02-01 22:50:05 +0300

    add errorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import clsx from 'clsx'
 import styles from './App.module.css'
 import { useThemeStore } from './stores/themeStore'
 import ToastContainer from "./components/ToastContainer/ToastContainer.tsx";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary.tsx";
 
 function App() {
   const { theme } = useThemeStore()
@@ -17,16 +18,18 @@ function App() {
 
   return (
     <Router>
-      <Header />
-      <main className={clsx('container', styles.main)}>
-        <Routes>
-          <Route path="/" element={<CatalogPage />} />
-          <Route path="/products" element={<CatalogPage />} />
-          {/* <Route path="/products/:id" element={<ProductPage />} /> */}
-          <Route path="/cart" element={<CartPage />} />
-        </Routes>
-      </main>
-      <ToastContainer />
+      <ErrorBoundary>
+        <Header/>
+        <main className={clsx('container', styles.main)}>
+          <Routes>
+            <Route path="/" element={<CatalogPage/>}/>
+            <Route path="/products" element={<CatalogPage/>}/>
+            {/* <Route path="/products/:id" element={<ProductPage />} /> */}
+            <Route path="/cart" element={<CartPage/>}/>
+          </Routes>
+        </main>
+        <ToastContainer/>
+      </ErrorBoundary>
     </Router>
   )
 }
---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,56 @@
+// components/ErrorBoundary.tsx — улучшенная версия с retry
+import React from 'react';
+import styles from './ErrorBoundary.module.scss'
+
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+  fallback: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+  error: Error | null;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = {hasError: false, error: null};
+  }
+
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    return {hasError: true, error};
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.error('ErrorBoundary caught:', error, errorInfo);
+  }
+
+  retry = () => {
+    this.setState({hasError: false, error: null});
+  };
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className={styles["error-boundary"]}>
+
+          <div className={styles["error-boundary__box"]}>
+            <h3>⚠️ Что-то пошло не так</h3>
+            <p>{this.state.error?.message}</p>
+            <button
+              onClick={this.retry}
+              className={styles["error-boundary__btn"]}
+            >
+              Попробовать снова
+            </button>
+          </div>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
